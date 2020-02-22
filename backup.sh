12c1b888a7d7c5b91601363d18d53bc6036229c4
houtknots <houtknots@users.noreply.github.com>
2020-02-22 23:19:58 +0100

    Functions + Features
    Adding Functions to call back the some settings at a later stage.
    
    Added features like cronjobs,  and the option to revisit your entered settings before it all is installed

--- backup.sh
+++ backup.sh
@@ -1,10 +1,94 @@
+#!/bin/sh
+
+# Project: Rclone backup script
+# Author : houtknots
+# Website: https://houtknots.nl/
+# Github : https://github.com/houtknots
+
+##########################################################
+##########################################################
+##########################################################
+
 localfolder=/home/
-remotefolder=/
+remotefolder=/backupscript/
+currentdate=$(date +"%Y-%m-%d_%H-%M-%S")
+tempfolder="/etc/backup/backupscript/temp"
+tempfile="$tempfolder/$currentdate.zip"
+retention="false"
+retention_daystostore="14"
 
-#Clear Log Files
-echo " " > /etc/backup/backupscript/backup.log
+function_timer () {
+	#Countdown timer before starting backup
+	secs=$((10))
+	while [ $secs -gt 0 ]; do
+		echo -ne -e "${GREEN}Backup starts in ${RED}$secs ${GREEN}seconds${NC} - ${GREEN}Press ${RED}Control+C ${GREEN}to cancel\033[0K\r ${NC}"
+		sleep 1
+		: $((secs--))
+	done
+	clear
+}
 
-#Backup the files to a remote location
-rclone sync $localfolder backupscript:$remotefolder --progress -q --log-file=/etc/backup/backupscript/backup.log
+function_clearlog () {
+	#Clear Log Files
+	echo " " > /etc/backup/backupscript/backup.log
+}
+
+function_createfolder () {
+	#Check if temp folder exists if not create it
+	if [ ! -d "$tempfolder" ]; then	
+		echo -e "${YELLOW}Creating Temp folder...${NC}"
+		mkdir $tempfolder
+		sleep 1
+	fi
+}
+
+function_zipfiles () {
+	zip -r $tempfile $localfolder
+}
+
+function_upload () {
+	#Backup the files to a remote location
+	rclone copy $tempfile backupscript:$remotefolder --progress -q --log-file=/etc/backup/backupscript/backup.log
+}
+
+function_deltempfiles () {
+#Delete temp files when done
+	if [ -f "$tempfile" ];
+	then
+		rm -rf $tempfile
+		if [ -f "$tempfile" ];
+		then
+			echo -e "${RED}Cleanup failed${NC} - Start the script again with ${YELLOW}-c ${NC}To restart the cleanup proccess"
+			echo -e "${RED}Cleanup failed${NC} - Start the script again with ${YELLOW}-c ${NC}To restart the cleanup proccess" > /etc/backup/backupscript/backup.log
+			exit 1
+		else
+			echo -e "${GREEN}Cleanup Succesfull${NC}"
+		fi
+	else
+		echo -e "${RED}No File found or we had insufficient permissions${NC} - Run the script again with ${YELLOW}-c ${NC}To restart the cleanup proccess"
+		echo -e "${RED}No File found or we had insufficient permissions${NC} - Run the script again with ${YELLOW}-c ${NC}To restart the cleanup proccess" > /etc/backup/backupscript/backup.log
+	fi
+}
+
+function_retention () {
+	rclone delete backupscript:$remotefolder --min-age $retention_daystostore\d --progress -q --log-file=/etc/backup/backupscript/retention.log
+}
+
+#########################################################
+#                Start the backupscript                 #
+#########################################################
+
+
+function_timer
+function_clearlog
+function_createfolder
+function_zipfiles
+
+function_upload
+function_deltempfiles
+
+if [ "$retention" == "true" ]; then
+	function_retention
+fi
 
 exit 0
--- installer.sh
+++ installer.sh
@@ -3,7 +3,7 @@
 # Project: Rclone backup script
 # Author : houtknots
 # Website: https://houtknots.nl/
-# Github : https://github.com/houtknots/Backupscript/
+# Github : https://github.com/houtknots
 
 ##########################################################
 ##########################################################
@@ -14,113 +14,307 @@ RED='\033[1;31m'				#Color Red in echo
 GREEN='\033[1;32m'				#Color GREEN in echo
 CYAN='\e[36m'					#Color CYAN in echo
 YELLOW='\e[33m'					#Color YELLOW in echo
+BLUE='\e[34m'					#Color BLUE in echo
 NC='\033[0m'					#Remove Colors in echo
 
 #Check the date
-DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-
-#Check to OS and declare the package installer
-declare -A osInfo;																														
-	osInfo[/etc/redhat-release]=yum
-	osInfo[/etc/arch-release]=pacman
-	osInfo[/etc/gentoo-release]=emerge
-	osInfo[/etc/debian_version]=apt-get
-
-for f in ${!osInfo[@]}
-do
-    if [[ -f $f ]];then
-        echo Package manager: ${osInfo[$f]}
-        PKGINSTALLER="${osInfo[$f]}"
-    fi
-done
+#DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 
 #Check if the user is root or runs the script with sudo
 if [[ $EUID -ne 0 ]]; then
-   echo -e "${RED}Please run this script as root ${NC}- Try to run with sudo or as root user" 				
+   echo -e "${RED}Please run this script as root ${NC}- Try to run with sudo or as root user"
    exit 1
 fi
 
-#Install required packages
-$PKGINSTALLER install zip -y
-$PKGINSTALLER install curl -y
-curl https://rclone.org/install.sh | sudo bash
+function_installpackages () {
+	#Install required packages
+	$PKGINSTALLER install zip -y
+	$PKGINSTALLER install unzip -y
+	$PKGINSTALLER install curl -y
+	curl https://rclone.org/install.sh | sudo bash
 
-sleep 1
+	#Check to OS and declare the package installer
+	declare -A osInfo;
+		osInfo[/etc/redhat-release]=yum
+		osInfo[/etc/arch-release]=pacman
+		osInfo[/etc/gentoo-release]=emerge
+		osInfo[/etc/debian_version]=apt-get
 
-#Check if Backup Folder exists if not create it
-if [ ! -d "/etc/backup" ]; then																												
-        echo -e "${YELLOW}Creating Backup folder...${NC}"
-	mkdir /etc/backup
-fi
-#Check if SFTP Folder exists if not create it
-if [ ! -d "/etc/backup/backupscript" ]; then																										
-        echo -e "${YELLOW}Creating SFTP folder...${NC}"
-        mkdir /etc/backup/backupscript
-fi
-#Check if temp folder exists if not create it
-if [ ! -d "/etc/backup/backupscript/temp" ]; then																									
-	echo -e "${YELLOW}Creating Temp folder...${NC}"
-	mkdir /etc/backup/backupscript/temp
-fi
+	for f in ${!osInfo[@]}
+	do
+    	if [[ -f $f ]];then
+        	echo Package manager: ${osInfo[$f]}
+        	PKGINSTALLER="${osInfo[$f]}"
+	    fi
+	done	
+}
 
-#Ask the user which protocol he or she wants to use for the file transfer
-echo -e "${YELLOW}Please select the protocol you would like to use.?${NC}"
-echo -e " [1] - WebDav"
-echo -e " [2] - SFTP"
-echo -e " [3] - FTP"
-echo -e " "
-read -p '[PROTOCOL]: ' protocol
-if [ "$protocol" != "${protocol#[1]}" ] ;then
-    echo -e "${GREEN}Webdav Selected${NC}"
-	rclone config create backupscript webdav
-elif [ "$protocol" != "${protocol#[2]}" ] ;then
-    echo -e "${GREEN}SFTP Selected${NC}"
-	rclone config create backupscript sftp
-elif [ "$protocol" != "${protocol#[3]}" ] ;then
-	echo -e "${GREEN}FTP Selected${NC}"
-	rclone config create backupscript ftp
-else
-	echo -e "${GREEN}Default SFTP Selected${NC}"
-	rclone config create backupscript sftp
-fi
-
-
-#Ask for the hostname
-echo -e "${YELLOW}Please enter your ${CYAN}remote hostname or IP ${YELLOW}and press enter${NC}"
-	read -p '[HOSTNAME]: ' hostname
-	rclone config update backupscript host $hostname
-	rclone config update backupscript url $hostname
-
-#Ask for the SFTP User
-echo -e "${YELLOW}Please enter your ${CYAN}remote user ${YELLOW}and press enter${NC}"	
-	read -p '[USERNAME]: ' username
-	rclone config update backupscript user $username
-
-#Ask for the SFTP Password
-echo -e "${YELLOW}Please enter your ${CYAN}remote password${NC} ${YELLOW}and press enter${NC}"		
-	read -sp '[PASSWORD]: ' password
-	rclone config password backupscript pass $password
-
-#Update vendor to other, this option is not important for this script
-rclone config update backupscript vendor other
-
-#Ask which files to backup
-echo -e "${YELLOW}Please enter the ${CYAN}local file location${NC} ${YELLOW}you want to backup and press enter${NC}"
-	read -p '[LOCAL FOLDER]: ' localfolder
-	if [ $localfolder == NULL ]; then 
-		localfolder = /home/
+function_createfolders () {
+	#Check if needed folders exist and create them if they do not exist
+	if [ ! -d "/etc/backup" ]; then
+        	echo -e "${YELLOW}Creating Backup folder...${NC}"
+		mkdir /etc/backup
 	fi
-	sed -i "s|^localfolder=.*|localfolder=${localfolder}|g" /etc/backup/backupscript/backup.sh
-	
-#Ask which files to backup
-echo -e "${YELLOW}Please enter the ${CYAN}remote folder${NC} ${YELLOW}you want to put the backup${NC}"
-	read -p '[REMOTE FOLDER]: ' remotefolder
-	if [ $remotefolder == NULL ]; then 
-		remotefolder = /
+	if [ ! -d "/etc/backup/backupscript" ]; then
+        	echo -e "${YELLOW}Creating SFTP folder...${NC}"
+        	mkdir /etc/backup/backupscript
 	fi
-	sed -i "s|^remotefolder=.*|remotefolder=${remotefolder}|g" /etc/backup/backupscript/backup.sh
+	if [ ! -d "/etc/backup/backupscript/temp" ]; then
+		echo -e "${YELLOW}Creating Temp folder...${NC}"
+		mkdir /etc/backup/backupscript/temp
+	fi
+	sleep 1
+	clear
+}
+
+function_downloadbackupscript () {
+	#Download backupscript from github
+	if [ ! -f "/etc/backup/backupscript/backup.sh" ]; then
+		curl https://raw.githubusercontent.com/houtknots/backupscript-rclone/master/backup.sh -o /etc/backup/backupscript/backup.sh
+	fi
+}
+
+#ask if the user wants to purge the old config, otherwise abort script
+function_newconfig () {
+	while [ "$newconfig_continue" != "true" ]; do
+		echo -e "${YELLOW}Would you like to continue, this will remove your current rclone config!?${NC}"
+		read -p '[OVERWRITE CONFIG] (y/n): ' newconfig
+			case $newconfig in
+  			y|Y) 
+				newconfig_continue="true"
+				rclone config delete backupscript
+			;;
+	  		n|N)
+				echo "Please use the commands to edit the rclone config"
+				newconfig_continue="true"
+				exit 0
+			;;
+  			*) 	
+				echo "Please enter y or n"
+				newconfig_continue="false"
+			;;  
+			esac
+	done
+	clear
+}
+
+function_protocol () {
+	#Ask the user which protocol he or she wants to use for the file transfer
+	echo -e "${YELLOW}Please select the protocol you would like to use.?${NC}"
+	echo -e " [1] - WebDav"
+	echo -e " [2] - SFTP"
+	echo -e " [3] - FTP"
+	echo -e " "
+	read -e -p '[PROTOCOL]: ' -i "$protocol" protocol
+	clear
+}
+
+function_hostname () {
+	#Ask for the hostname
+	echo -e "${YELLOW}Please enter your ${CYAN}remote hostname or IP ${YELLOW}and press enter${NC}"
+		read -e -p '[HOSTNAME]: ' -i "$hostname" hostname
+	clear
+}
+
+function_username () {
+	#Ask for the remote destination username
+	echo -e "${YELLOW}Please enter your ${CYAN}remote user ${YELLOW}and press enter${NC}"	
+		read -e -p '[USERNAME]: ' -i "$username" username
+	clear
+}
+
+function_password () {
+	#Ask for the remote destination password
+	echo -e "${YELLOW}Please enter your ${CYAN}remote password${NC} ${YELLOW}and press enter${NC}"		
+		read -sp '[PASSWORD]: ' password
+	clear
+}
+
+function_localfolder () {
+	#Ask which files to backup
+	echo -e "${YELLOW}Please enter the ${CYAN}local file location${NC} ${YELLOW}you want to backup and press enter${NC}"
+		read -e -p '[LOCAL FOLDER]: ' -i "/home/" localfolder
+	clear
+}
+
+function_tempfolder () {
+	#Ask which files to backup
+	echo -e "${YELLOW}Please enter the ${CYAN}local temporary location${NC} ${YELLOW}the backup will use before uploading and press enter${NC}"
+		read -e -p '[LOCAL TEMPORARY FOLDER]: ' -i "/etc/backup/backupscript/temp" tempfolder
+	clear
+}
+
+function_remotefolder () {
+	#Ask where to put the files on the remote side
+	echo -e "${YELLOW}Please enter the ${CYAN}remote folder${NC} ${YELLOW}you want to put the backup${NC}"
+		read -e -p '[REMOTE FOLDER]: ' -i "/backupscript/" remotefolder
+	clear
+}
+
+function_retention () {
+	#Ask if the user wants to use retention on the remote server side
+	while [ "$retention_continue" != "true" ]; do
+	echo -e "${YELLOW}Would you like to purge old backups with the ${CYAN}retention${NC} ${YELLOW}feature? ${NC}"
+		read -p '[RETENTION] (y/n): ' retention
+			case $retention in
+  			y|Y)
+				retention_continue="true"
+				retention_value="true"
+				echo -e "${YELLOW}How many ${CYAN}days${NC} ${YELLOW}would you like to store the old backups before purging them?${NC}"
+				read -e -p '[DAYS TO KEEP]: ' -i "14" retention_daystostore
+			;;
+	  		n|N)
+				echo "Skipping Retention"
+				retention_continue="true"
+				retention_value="false"
+				retention_daystostore="disabled"
+			;;
+  			*) 
+				retention_continue="false"
+			;;
+			esac
+		clear	
+	done
+}
+
+function_cronjob () {
+	#Ask if the user wants to install a daily backup
+	while [ "$cronjob_continue" != "true" ]; do
+	echo -e "${YELLOW}Would you like to install a ${CYAN}Daily cronjob${NC} ${YELLOW}for automatic backups? ${NC}"
+		read -p '[RETENTION] (y/n): ' retention
+			case $retention in
+  			y|Y)
+				cronjob_continue="true"
+				cronjob_install="true"
+			;;
+	  		n|N)
+				cronjob_continue="true"
+				cronjob_install="false"
+			;;
+  			*) 
+				cronjob_continue="false"
+				cronjob_install="false"
+			;;
+			esac
+		clear	
+	done
+}
+}
+
+
+#########################################################
+#                  Configure Settings                   #
+#########################################################
+
+#Start the settings configuration
+function_settings () {
+	function_protocol
+	function_hostname
+	function_username
+	function_password
+	function_localfolder
+	function_localtempfolder
+	function_remotefolder
+	function_retention
+	function_cronjob
+}
+
+#Ask if the user wants to create a new config
+function_preparesystem () {
+	function_installpackages
+	function_createfolders
+	function_downloadbackupscript
+	function_newconfig
+}
+
+#Start the settings function
+function_preparesystem
+function_settings
+
+#########################################################
+#                  Install Components                   #
+#########################################################
+while [ "$confirm_settings_continue" != "true" ]; do
+	#Ask user if the settings are correct and install components
+	echo -e "[PROTOCOL]: $protocol"
+	echo -e "[HOSTNAME]: $hostname"
+	echo -e "[USERNAME]: $username"
+	echo -e "[PASSWORD]: *Hidden*"
+	echo -e "[LOCAL FOLDER]: $localfolder "
+	echo -e "[LOCAL TEMPORY FOLDER]: $tempfolder"
+	echo -e "[REMOTE FOLDER]: $remotefolder"
+	echo -e "[RETENTION]: $cronjob_install"
+	if [ "retention_value" == "true" ]; then
+		echo -e "[RETENTION DAYS TO KEEP]: $retention_daystostore"
+	fi
+	echo -e "[DAILY CRONJOB]: $cronjob"
+	echo -e " "
+	read -p 'Are the above values correct? (y/n): ' confirm_settings
+		case $confirm_settings in
+  		y|Y)
+			confirm_settings_continue="true"
+			confirm_settings_install="true"
+		;;
+	  	n|N)
+			confirm_settings_continue="false"
+			confirm_settings_install="false"
+			echo -e "Please revisit your settings"
+			2
+			function_settings
+		;;
+  		*) 
+			confirm_settings_continue="false"
+			confirm_settings_install="false"
+		;;
+		esac
+	clear	
+
+done
+
+if [ $confirm_settings_install == "true" ]; then
+	#Create the RCLONE config with the selected protocol
+	if [ "$protocol" != "${protocol#[1]}" ] ;then
+	    echo -e "${GREEN}Webdav Selected${NC}"
+		rclone config create backupscript webdav
+	elif [ "$protocol" != "${protocol#[2]}" ] ;then
+	    echo -e "${GREEN}SFTP Selected${NC}"
+		rclone config create backupscript sftp
+	elif [ "$protocol" != "${protocol#[3]}" ] ;then
+		echo -e "${GREEN}FTP Selected${NC}"
+		rclone config create backupscript ftp
+	else
+		echo -e "${GREEN}Default SFTP Selected${NC}"
+		rclone config create backupscript sftp
+	fi
+
+	#Set the RClone Settings in the config file
+	rclone config update backupscript host $hostname #Set rclone config host
+	rclone config update backupscript url $hostname #set rclone config url
+	rclone config update backupscript user $username #set rclone config username
+	rclone config password backupscript pass $password #set rclone config password
+	rclone config update backupscript vendor other #set rclone config vendor
+
+	#Edit the backup.sh file
+	sed -i "s|^localfolder=.*|localfolder=${localfolder}|g" /etc/backup/backupscript/backup.sh #local folder
+	sed -i "s|^tempfolder=.*|tempfolder=${tempfolder}|g" /etc/backup/backupscript/backup.sh #temp folder
+	sed -i "s|^remotefolder=.*|remotefolder=${remotefolder}|g" /etc/backup/backupscript/backup.sh #remote folder
+	sed -i "s|^retention=.*|retention=${retention_value}|g" /etc/backup/backupscript/backup.sh #retention
+	sed -i "s|^retention_daystostore=.*|retention_daystostore=${retention_daystostore}|g" /etc/backup/backupscript/backup.sh #retention
+
+	if [ "$cronjob_install" == "true" ]; then
+		touch /etc/cron.d/backupscript
+		echo "0 3 * * * root bash /etc/backup/backupscript/backup.sh" >> /etc/cron.d/backupscript
+		systemctl restart crond
+	fi
+fi
+
+#########################################################
+#              Notify User Install Is Done              #
+#########################################################
+
 
 #Echo the user how to test the backup
 echo -e "${GREEN}The backup-script is installed use the following command to run the script ${YELLOW}bash /etc/backup/backupscript/backup.sh${NC}"
+echo -e "${GREEN}If you would like the backup to run automaticly you will need to add a cronjob for this."
 
 exit 0
